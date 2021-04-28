ba25954924ba49fec2da75fba91ce3aede5bb5df
Juan Jose Figueroa Hurtatis <jfigueroa@aaglobalc.com>
2021-04-27 23:32:51 -0500

    Creacion de la pagina de inicio

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:medidation_app_flutter/constants.dart';
+import 'package:medidation_app_flutter/widgets/bottom_nav_bar.dart';
+import 'package:medidation_app_flutter/widgets/category_card.dart';
 
 void main() {
   runApp(MyApp());
@@ -25,8 +28,79 @@ class MyApp extends StatelessWidget {
 class HomeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
-      
+      bottomNavigationBar: BottomNavBar(),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: size.height * .45,
+            decoration: BoxDecoration(
+              color: Color(0xFFF5CEB8),
+              image: DecorationImage(
+                alignment: Alignment.centerLeft,
+                image: AssetImage("assets/images/undraw_pilates_gpdb.png"),
+              ),
+            ),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 52,
+                      width: 52,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF2BEA1),
+                        shape: BoxShape.circle
+                      ),
+                      child: SvgPicture.asset("assets/icons/menu.svg"),
+                    ),
+                  ),
+                  Text(
+                    "Good Morning \nShishir",
+                    style: Theme.of(context).textTheme.display1.copyWith(fontWeight: FontWeight.w900),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 30),
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(29.5)
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Search",
+                        icon: SvgPicture.asset("assets/icons/search.svg"),
+                        border: InputBorder.none
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: GridView.count(
+                      crossAxisCount: 2,
+                      childAspectRatio: .85,
+                      crossAxisSpacing: 20,
+                      mainAxisSpacing: 20,
+                      children: <Widget>[
+                        CategoryCard( title: "Diet Recommendation", svgSrc: "assets/icons/Hamburger.svg", press: (){},),
+                        CategoryCard( title: "Kegel Exercises", svgSrc: "assets/icons/Excrecises.svg", press: (){},),
+                        CategoryCard( title: "Meditation", svgSrc: "assets/icons/Meditation.svg", press: (){},),
+                        CategoryCard( title: "Yoga", svgSrc: "assets/icons/yoga.svg", press: (){},),
+                      ], 
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
